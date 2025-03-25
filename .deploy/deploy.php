de382bfedcd4e5e09702cf3fbd09a22d8f2fdc37
Aria Gholami <ariagholami@web.de>
2025-03-25 18:48:21 +0100

    ci: Use sulu deployer php recipe

--- .deploy/deploy.php
+++ .deploy/deploy.php
@@ -2,31 +2,19 @@
 
 declare(strict_types=1);
 
-namespace Xing\Deployer;
+namespace Deployer;
 
-use function Deployer\import;
-use function Deployer\runLocally;
-use function Deployer\set;
-use function Deployer\task;
-
-require_once  'recipe/common.php';
+require 'recipe/sulu.php';
 
 import(__DIR__ . '/hosts.yaml');
 
-set('shared_files', [
+add('shared_files', [
     '.env',
     'public/.htaccess',
     'public/robots.txt'
 ]);
-set('shared_dirs', [
-    'public/uploads',
-    'var/uploads',
-    'var/log'
-]);
-set('writable_dirs', [
-    'var',
-    'public/uploads'
-]);
+add('shared_dirs', []);
+add('writable_dirs', []);
 set('do_not_deploy', [
     '.ddev',
     '.deploy',
@@ -51,8 +39,24 @@ task('local:npm:build', static function (): void {
     runLocally('npm run build');
 });
 
-task('deploy', [
+task('deploy:prepare', [
     'local:composer:install',
     'local:npm:install',
-    'local:npm:build'
+    'local:npm:build',
+    'deploy:info',
+    'deploy:setup',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:env',
+    'deploy:shared',
+    'deploy:writable',
 ]);
+
+task('deploy', [
+    'deploy:prepare',
+    'deploy:cache:clear',
+    'deploy:publish',
+]);
+
+after('deploy:failed', 'deploy:unlock');
