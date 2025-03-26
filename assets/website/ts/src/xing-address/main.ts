4a281d2066c4cf21ae2e0169a4e8e90d68fe1a67
Aria Gholami <ariagholami@web.de>
2025-03-26 09:31:25 +0100

    feat: Add skeleton test environment for javascript

--- assets/website/ts/src/xing-address/main.ts
+++ assets/website/ts/src/xing-address/main.ts
@@ -1,13 +1,15 @@
-let address: Element = document.querySelector('.xing_address-container div a');
-address.addEventListener('click', (): void => {
-    if (address.textContent === 'Copied!') {
-        return;
-    }
+let address: Element|null = document.querySelector('.xing_address-container div a');
+if (address instanceof Element) {
+    address.addEventListener('click', (): void => {
+        if (address.textContent === 'Copied!') {
+            return;
+        }
 
-    const addressText: string =  '5JcdnWEwuHh1v3SAARq8zH9tEwDQGpaHzBrZ81m4pump';
-    address.textContent = 'Copied!';
-    navigator.clipboard.writeText(addressText);
-    setTimeout(function(): void {
-        address.textContent = '📋 5JcdnWEwuHh1v3SAARq8zH9tEwDQGpaHzBrZ81m4pump';
-    }, 1000);
-});
+        const addressText: string =  '5JcdnWEwuHh1v3SAARq8zH9tEwDQGpaHzBrZ81m4pump';
+        address.textContent = 'Copied!';
+        navigator.clipboard.writeText(addressText);
+        setTimeout(function(): void {
+            address.textContent = '📋 5JcdnWEwuHh1v3SAARq8zH9tEwDQGpaHzBrZ81m4pump';
+        }, 1000);
+    });
+}
