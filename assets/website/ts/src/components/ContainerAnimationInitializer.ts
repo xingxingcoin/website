2ecd871075e2838fee7e923b777d4093fe8a8299
Aria Gholami <ariagholami@web.de>
2025-03-29 11:55:54 +0100

    feat: Use changeClass function in first load

--- assets/website/ts/src/components/ContainerAnimationInitializer.ts
+++ assets/website/ts/src/components/ContainerAnimationInitializer.ts
@@ -3,14 +3,20 @@ export default class ContainerAnimationInitializer {
     {
         const hiddenContainerCollection: NodeList = document.querySelectorAll('.container-left-hidden, .container-bottom-hidden');
         Array.from(hiddenContainerCollection).forEach((hiddenContainer: any): void => {
+            this.changeClass(hiddenContainer);
             window.addEventListener('scroll', (): void => {
-                const rect: DOMRect = hiddenContainer.getBoundingClientRect();
-                if (rect.top < window.innerHeight * 0.9) {
-                    hiddenContainer.classList.add('container-show');
-                } else {
-                    hiddenContainer.classList.remove('container-show');
-                }
+                this.changeClass(hiddenContainer);
             });
         });
+    }
+
+    private changeClass(hiddenContainer: any): void
+    {
+        const rect: DOMRect = hiddenContainer.getBoundingClientRect();
+        if (rect.top < window.innerHeight * 0.9) {
+            hiddenContainer.classList.add('container-show');
+        } else {
+            hiddenContainer.classList.remove('container-show');
+        }
     }
 }
