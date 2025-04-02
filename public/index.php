3b2f2d45282ca77c2eadf7f43437603436b4b67e
Aria Gholami <ariagholami@web.de>
2025-04-02 13:46:47 +0200

    feat: Move umask to top of index.php

--- public/index.php
+++ public/index.php
@@ -17,6 +17,7 @@ use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
+umask(002);
 \defined('SULU_MAINTENANCE') || \define('SULU_MAINTENANCE', \getenv('SULU_MAINTENANCE') ?: false);
 
 // maintenance mode
@@ -32,7 +33,6 @@ require \dirname(__DIR__) . '/vendor/autoload.php';
 
 (new Dotenv())->bootEnv(\dirname(__DIR__) . '/.env');
 
-umask(002);
 if ($_SERVER['APP_DEBUG']) {
     \umask(0000);
 
